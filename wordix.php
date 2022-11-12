98963e8f5017d6a9f67b31b6610d50c5ec1753a6
MarcosAbiG <marcos.abi@est.fi.uncoma.edu.ar>
2022-11-12 12:18:22 -0300

    menu

--- wordix.php
+++ wordix.php
@@ -1,2 +1,11 @@
 <?
-echo "prueba";
+
+echo "1) Jugar al Wordix con una palabra elegida
+2) Jugar al Wordix con una palabra aleatoria
+3) Mostrar una partida
+4) Mostrar la primer partida ganadora
+5) Mostrar resumen de Jugador
+6) Mostrar listado de partidas ordenadas por jugador y por palabra
+7) Agregar una palabra de 5 letras a Wordix
+8) Salir";
+;
